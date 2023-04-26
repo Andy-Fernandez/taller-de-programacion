6b8eb2051fd37d575f46c9846115692acb377127
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-04-26 18:09:19 -0400

    cambios varios

--- src/Tareas/Tarea5/ejercicio5.java
+++ src/Tareas/Tarea5/ejercicio5.java
@@ -1,26 +1,26 @@
-package Tareas.Tarea5;
-import java.lang.reflect.Array;
+    package Tareas.Tarea5;
+    import java.lang.reflect.Array;
 
-public class ejercicio5 {
-    public static void main(String[] args) {
-        // Ejemplo de uso
+    public class ejercicio5 {
+        public static void main(String[] args) {
+            // Ejemplo de uso
 
-        String[] miVector3 = {"Hola", "Mundo", "Como", "Estas", "Hoy", "Esta", "Es", "La", "Tarea", "De", "143"};
-        System.out.println(mostrarVector(miVector3));
-    }
-
-    //5. Dado un vector de cadenas, buscar la cadena x.
-
-
-    // Función para mostrar un vector
-    public static String mostrarVector(Object vector) {
-        StringBuilder vectorString = new StringBuilder();
-        int longitud = Array.getLength(vector);
-        for (int i = 0; i < longitud; i++) {
-            vectorString.append(Array.get(vector, i)).append(" ");
+            String[] miVector3 = {"Hola", "Mundo", "Como", "Estas", "Hoy", "Esta", "Es", "La", "Tarea", "De", "143"};
+            System.out.println(mostrarVector(miVector3));
         }
-        return vectorString.toString();
-    }
 
-}
+        //5. Dado un vector de cadenas, buscar la cadena x.
+
+
+        // Función para mostrar un vector
+        public static String mostrarVector(Object vector) {
+            StringBuilder vectorString = new StringBuilder();
+            int longitud = Array.getLength(vector);
+            for (int i = 0; i < longitud; i++) {
+                vectorString.append(Array.get(vector, i)).append(" ");
+            }
+            return vectorString.toString();
+        }
+
+    }
 
